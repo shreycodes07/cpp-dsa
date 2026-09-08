c7bda83b15425fdab145c14d8b65519671c76da8
shreycodes07 <shreyal1240@gmail.com>
2026-09-08 21:49:11 +0530

    Create number_pyramid.cpp

---
+++ patterns/number_pyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter no. of lines ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+
+        // spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // numbers
+        for (int j = 1; j <= i; j++) {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
